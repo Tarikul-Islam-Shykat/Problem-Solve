f9f2588e4af03c1ef45f084aeb53cd8bf46c7040
Tarikul-Islam-Shykat <77191261+Tarikul-Islam-Shykat@users.noreply.github.com>
2021-09-04 12:12:27 +0600

    Create SolveMeFirst.java

---
+++ Problem Solve/SolveMeFirst.java
@@ -0,0 +1,12 @@
+
+import java.util.Scanner;
+public class Solution {
+ public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        int b = in.nextInt();
+        int sum;
+        sum = a+b;
+        System.out.println(sum);
+   }
+}
