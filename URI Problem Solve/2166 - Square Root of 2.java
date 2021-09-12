df8a9daed7aa8dc3ed0a27cf0441a9ec60b0f11c
Shykat Islam <77191261+Tarikul-Islam-Shykat@users.noreply.github.com>
2021-09-13 03:28:54 +0600

    Create  2166 - Square Root of 2.java

---
+++ URI Problem Solve/2166 - Square Root of 2.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double N = sc.nextDouble();
+        
+        double num =0;
+        for(int i=0; i<N; i++)
+        {
+            num=num+2.0;
+            num=(1.0/num);
+        
+        }
+        num=num+1.0;
+        
+        System.out.printf("%.10f\n",num);
+        
+     
+    }
+    
+}
