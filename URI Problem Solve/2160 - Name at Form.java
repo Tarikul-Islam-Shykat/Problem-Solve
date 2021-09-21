9b29dd9dd5bee4435b7ca2b7f6a984b2947ad39a
Shykat Islam <77191261+Tarikul-Islam-Shykat@users.noreply.github.com>
2021-09-21 23:15:56 +0600

    Create  2160 - Name at Form.java

---
+++ URI Problem Solve/2160 - Name at Form.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+                if(s.length()<=80)
+                {System.out.println("YES");}
+                else
+                {
+                    System.out.println("NO");
+                }
+                
+        
+    }
+    
+}
