02add9087fa4613ce0bb2761552d9f6b6b54836b
Shykat Islam <77191261+Tarikul-Islam-Shykat@users.noreply.github.com>
2021-10-26 03:34:53 +0600

    Create  1173 - Array fill I.py

---
+++ URI Problem Solve/Python/1173 - Array fill I.py
@@ -0,0 +1,6 @@
+input_number_of_1st_array=int(input())
+print("N[%d] = %d" % (0, input_number_of_1st_array))
+for counter in range(1, 10):
+    for_next_number = input_number_of_1st_array*2
+    print("N[%d] = %d" % (counter, for_next_number))
+    input_number_of_1st_array = for_next_numbe
