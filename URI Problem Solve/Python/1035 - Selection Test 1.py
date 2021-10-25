7b294f5d2f5c280748910fa525148e53b7d7ffc0
Shykat Islam <77191261+Tarikul-Islam-Shykat@users.noreply.github.com>
2021-10-26 03:46:26 +0600

    Create  1035 - Selection Test 1.py

---
+++ URI Problem Solve/Python/1035 - Selection Test 1.py
@@ -0,0 +1,6 @@
+lin1=input().split(" ")
+A, B, C, D=lin1
+if int(B)>int(C) and  int(D)>int(A) and (int(C)+int(D))>(int(A)+int(B)) and int(C)>0 and int(D)>0 and int(A)%2==0:
+    print("Valores aceitos")
+else:
+    print('Valores nao aceitos')
