b98a6222ed9ab965116ae4296f3c674eea075f27
Shykat Islam <77191261+Tarikul-Islam-Shykat@users.noreply.github.com>
2021-11-28 21:04:12 +0600

    Create 1007 - Difference.kt

---
+++ URI Problem Solve/Kotlin/1007 - Difference.kt
@@ -0,0 +1,16 @@
+import java.util.*
+
+fun main(args: Array<String>) {
+
+    val sc = Scanner(System.`in`)
+
+    var num1 = sc.nextInt();
+    var num2 = sc.nextInt();
+    var num3 = sc.nextInt();
+    var num4 = sc.nextInt();
+
+    var product  : Int = (num1 * num2 - num3 * num4)
+
+
+    println( "DIFERENCA = "+product)
+}
