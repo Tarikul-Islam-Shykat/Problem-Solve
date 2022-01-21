9abdce3ab668da5f38961968dc95a2882c58975c
Shykat Islam <77191261+Tarikul-Islam-Shykat@users.noreply.github.com>
2022-01-21 15:36:52 +0600

    Create List Comprehensions.py

---
+++ Hacker Rank/Python/Basic Data Types/List Comprehensions.py
@@ -0,0 +1,16 @@
+if __name__ == '__main__':
+    x = int(input())
+    y = int(input())
+    z = int(input())
+    n = int(input())
+
+    dic = []
+    result = []
+    for i in range(x+1):
+        for j in range(y+1):
+            for k in range(z+1):
+                if (i+j+k)!=n:
+                    dic = [i,j,k]
+                    result.append(dic)
+
+    print(result)
