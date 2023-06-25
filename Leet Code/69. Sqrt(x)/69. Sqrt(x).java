f7a9bb9e24ac8847b6d6657479f8f448fbcf7ad1
Tarikul Islam Shykat <77191261+Tarikul-Islam-Shykat@users.noreply.github.com>
2023-06-25 12:16:27 +0600

    Create 69. Sqrt(x).java

---
+++ Leet Code/69. Sqrt(x)/69. Sqrt(x).java
@@ -0,0 +1,16 @@
+import java.lang.Math;
+class Solution {
+    public int mySqrt(int x) {
+        int val = (int)(Math.sqrt(x));
+        return val;
+
+    }
+}
+
+
+/*
+Runtime 1 ms
+Beat 100%
+Memory 40 MB
+Beats 36.75%
+*/
