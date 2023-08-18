8eddf8e2471b3cd69d6e17c3a54a91132ae8f9ab
Tarikul Islam Shykat <77191261+Tarikul-Islam-Shykat@users.noreply.github.com>
2023-08-18 17:19:21 +0600

    Create the-blunder.sql

---
+++ Hacker Rank/SQL/the-blunder.sql
@@ -0,0 +1,4 @@
+select ceil(avg(salary) - avg(replace(salary, '0',''))) from EMPLOYEES ;
+
+ceil is for getting the celling value. 
+https://www.hackerrank.com/challenges/the-blunder/problem?isFullScreen=true
