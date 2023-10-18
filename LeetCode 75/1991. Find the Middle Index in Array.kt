de2a980d30ea95513a3b65fdd3f4586cc27e9e57
Tarikul Islam Shykat <77191261+Tarikul-Islam-Shykat@users.noreply.github.com>
2023-10-18 18:47:34 +0600

    Create 1991. Find the Middle Index in Array.kt

---
+++ LeetCode 75/1991. Find the Middle Index in Array.kt
@@ -0,0 +1,22 @@
+class Solution {
+    fun findMiddleIndex(nums: IntArray): Int {
+    var total_sum = nums.sum()
+    var arr_length = nums.size-1
+    var left_sum = 0
+
+    for (index in 0..arr_length){
+
+        var result = total_sum - left_sum - nums[index]
+
+        if(result == left_sum){
+            return  index
+        }
+
+        left_sum += nums[index]
+    }
+
+
+    return  -1
+
+    }
+}
