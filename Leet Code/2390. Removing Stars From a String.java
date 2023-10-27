3eac45600796da535357310d3bca915188d020e3
Tarikul Islam Shykat <77191261+Tarikul-Islam-Shykat@users.noreply.github.com>
2023-10-27 21:22:01 +0600

    Create 2390. Removing Stars From a String.java

---
+++ Leet Code/2390. Removing Stars From a String.java
@@ -0,0 +1,11 @@
+StringBuilder res = new StringBuilder();
+        for(char c : s.toCharArray()){
+            if(c =='*'){
+                res.setLength(res.length()-1);
+            }
+            else{
+                res.append(c);
+            }
+        }
+        
+        return res.toString(); 
