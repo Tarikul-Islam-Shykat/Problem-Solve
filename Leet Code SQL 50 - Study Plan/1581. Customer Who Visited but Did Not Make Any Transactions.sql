f25979147d974b041b7936cb22a1b3ce531ce1e7
Tarikul Islam Shykat <77191261+Tarikul-Islam-Shykat@users.noreply.github.com>
2023-12-26 21:34:32 +0600

    Create 1581. Customer Who Visited but Did Not Make Any Transactions.sql

---
+++ Leet Code SQL 50 - Study Plan/1581. Customer Who Visited but Did Not Make Any Transactions.sql
@@ -0,0 +1,7 @@
+
+select Visits.customer_id, count(Visits.visit_id) as 'count_no_trans'
+from Visits
+left join Transactions
+on Visits.visit_id = Transactions.visit_id
+where Transactions.visit_id is null
+group by customer_id;
