a76fed01584ffc8879906a7573c26d9576fca157
Tarikul Islam Shykat <77191261+Tarikul-Islam-Shykat@users.noreply.github.com>
2023-12-29 11:53:39 +0600

    Update 1280. Students and Examinations.sql

--- Leet Code SQL 50 - Study Plan/1280. Students and Examinations.sql
+++ Leet Code SQL 50 - Study Plan/1280. Students and Examinations.sql
@@ -1,8 +1,9 @@
 select a.student_id, a.student_name, b.subject_name, 
-count(c.subject_name) as attended_exams
+ifnull(count(c.subject_name),0) as attended_exams
 from
 Students as a
 cross join Subjects as b
 left join Examinations as c
 on a.student_id  = c.student_id  and b.subject_name = c.subject_name
-group by a.student_id, b.subject_name;
+group by a.student_id, b.subject_name
+order by a.student_id, b.subject_name;
