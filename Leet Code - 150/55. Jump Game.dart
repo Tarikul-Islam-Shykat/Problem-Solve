db99e20ec25c5b2bc5304ec49eb516b96ef10160
Tarikul Islam Shykat <77191261+Tarikul-Islam-Shykat@users.noreply.github.com>
2024-07-07 15:52:56 +0600

    Create 55. Jump Game.dart

---
+++ Leet Code - 150/55. Jump Game.dart
@@ -0,0 +1,11 @@
+class Solution {
+  bool canJump(List<int> nums) {
+    int final_pos_index  = nums.length - 1;
+     for(int idx = nums.length -2; idx >=0 ; idx--){
+        if(idx + nums[idx] >= final_pos_index){
+             final_pos_index = idx;
+        }
+     }
+     return final_pos_index == 0;
+  }
+}
